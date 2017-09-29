d41006b7e2afb9e5ee2087c48f2427839ecd61fc
Brandon Kudo <brandonkudo@gmail.com>
2017-09-29 11:45:13 -0700

    make prototypes for toppings and size, as well as constructor for pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,38 @@
+function Pizza(topping, size) {
+  this.pizzaTopping = topping;
+  this.pizzaSize = size;
+}
+
+Pizza.prototype.pizzaPrice = function () {
+  if (this.pizzaSize === "Small") {
+    return 8;
+  } else if (this.pizzaSize === "Medium") {
+    return 10;
+  } else {
+    return 12;
+  }
+}
+
+Pizza.prototype.toppingPrice = function () {
+  if (this.pizzaTopping === "Pepperoni") {
+    return 1;
+  } else if (this.pizzaTopping === "Combo") {
+    return 2;
+  } else {
+    return 0;
+  }
+}
+
+
+
+
+
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
     event.preventDefault();
+    var topping = $("#toppingInput").val();
+    var size = $("#sizeInput").val();
 
+    var customPizza = new Pizza(topping, size);
   });
 });
